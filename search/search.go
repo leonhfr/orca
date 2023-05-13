722f4308a5a7e1d6cc103ecc39e77fc7cff9d47e
Léon Hollender <hello@leonh.fr>
2023-05-13 23:18:11 +0200

    feat: search interface

--- search/search.go
+++ search/search.go
@@ -1,6 +1,12 @@
 package search
 
-import "math"
+import (
+	"context"
+	"math"
+
+	"github.com/leonhfr/orca/chess"
+	"github.com/leonhfr/orca/uci"
+)
 
 const (
 	// maxPkgDepth is the maximum depth at which the package will search.
@@ -10,3 +16,52 @@ const (
 	// draw is the score of a draw.
 	draw = 0
 )
+
+// Engine represents the search engine.
+type Engine struct{}
+
+// Search implements the UCI engine interface.
+func (Engine) Search(ctx context.Context, pos *chess.Position, maxDepth int) <-chan *uci.Output {
+	output := make(chan *uci.Output)
+
+	if maxDepth == 0 || maxDepth > maxPkgDepth {
+		maxDepth = maxPkgDepth
+	}
+
+	go func() {
+		defer close(output)
+		iterativeSearch(ctx, pos, maxDepth, output)
+	}()
+
+	return output
+}
+
+// iterativeSearch performs an iterative search.
+func iterativeSearch(ctx context.Context, pos *chess.Position, maxDepth int, output chan<- *uci.Output) {
+	for depth := 1; depth <= maxDepth; depth++ {
+		o, err := negamax(ctx, pos, depth)
+		if err != nil {
+			return
+		}
+		o.Mate = mateIn(o.Score)
+		output <- o
+	}
+}
+
+// mateIn returns the number of moves before mate.
+func mateIn(score int) int {
+	sign := sign(score)
+	delta := mate - sign*score
+	if delta > maxPkgDepth {
+		return 0
+	}
+	return sign * (delta/2 + delta%2)
+}
+
+// sign returns the sign +/-1 of the passed integer.
+func sign(n int) int {
+	if n < 0 {
+		return -1
+	}
+	return 1
+}
---
+++ search/search_test.go
@@ -0,0 +1,42 @@
+package search
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/leonhfr/orca/chess"
+	"github.com/leonhfr/orca/uci"
+)
+
+func TestSearch(t *testing.T) {
+	tests := []struct {
+		name  string
+		fen   string
+		depth int
+		oo    []uci.Output
+	}{
+		{
+			name:  "mate in 2",
+			fen:   "r1b1kb1r/pppp1ppp/2n1pq2/8/3Pn2N/2P3P1/PP1NPP1P/R1BQKB1R b KQkq - 3 6",
+			depth: 2,
+			oo: []uci.Output{
+				{Depth: 1, Nodes: 46, Score: 357, Mate: 0, PV: []chess.Move{0x2c322dc}},
+				{Depth: 2, Nodes: 1219, Score: 9223372036854775806, Mate: 1, PV: []chess.Move{0x2c1836d}},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := Engine{}.Search(context.Background(), unsafeFEN(tt.fen), tt.depth)
+			var outputs []uci.Output
+			for o := range output {
+				outputs = append(outputs, *o)
+			}
+
+			assert.Equal(t, tt.oo, outputs)
+		})
+	}
+}
