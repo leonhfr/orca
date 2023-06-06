7d55acb3813e8210efed51e8a4eab8fdadda9277
Léon Hollender <hello@leonh.fr>
2023-06-06 17:18:17 +0200

    refactor: heuristics

--- chess/position.go
+++ chess/position.go
@@ -2,7 +2,6 @@ package chess
 
 import (
 	"fmt"
-	"math/bits"
 	"strings"
 )
 
@@ -154,65 +153,6 @@ func (pos *Position) UnmakeMove(m Move, meta Metadata, hash, pawnHash Hash) {
 	pos.fullMoves = meta.fullMoves()
 	pos.hash = hash
 	pos.pawnHash = pawnHash
-}
-
-// CountPieces returns the count of knights, bishops, rooks, and queens.
-func (pos *Position) CountPieces() (int, int, int, int) {
-	return pos.board.bbPieces[Knight].ones(), pos.board.bbPieces[Bishop].ones(), pos.board.bbPieces[Rook].ones(), pos.board.bbPieces[Queen].ones()
-}
-
-// PieceMap executes the callback for each piece on the board, passing the piece
-// and its square as arguments.
-//
-// Does not take pawns into account.
-//
-// Intended to be used in evaluation functions.
-func (pos *Position) PieceMap(cb func(p Piece, sq Square)) {
-	for p, bb := range [8]bitboard{
-		pos.board.bbColors[Black] & pos.board.bbPieces[Knight],
-		pos.board.bbColors[White] & pos.board.bbPieces[Knight],
-		pos.board.bbColors[Black] & pos.board.bbPieces[Bishop],
-		pos.board.bbColors[White] & pos.board.bbPieces[Bishop],
-		pos.board.bbColors[Black] & pos.board.bbPieces[Rook],
-		pos.board.bbColors[White] & pos.board.bbPieces[Rook],
-		pos.board.bbColors[Black] & pos.board.bbPieces[Queen],
-		pos.board.bbColors[White] & pos.board.bbPieces[Queen],
-	} {
-		piece := Piece(p + 2)
-		for ; bb > 0; bb = bb.resetLSB() {
-			cb(piece, bb.scanForward())
-		}
-	}
-}
-
-// UniquePieceMap executes the callback for each piece on the board that do not
-// have an opponent mirrored piece, passing the piece and its square as arguments.
-//
-// Does not take pawns into account.
-//
-// Intended to be used in evaluation functions.
-func (pos *Position) UniquePieceMap(cb func(p Piece, sq Square)) {
-	bbBlack, bbWhite := pos.board.bbColors[Black], pos.board.bbColors[White]
-	bbQueen := pos.board.bbPieces[Queen]
-	bbRook := pos.board.bbPieces[Rook]
-	bbBishop := pos.board.bbPieces[Bishop]
-	bbKnight := pos.board.bbPieces[Knight]
-
-	for p, bb := range [8]bitboard{
-		^bitboard(bits.ReverseBytes64(uint64(bbWhite&bbKnight))) & bbBlack & bbKnight,
-		^bitboard(bits.ReverseBytes64(uint64(bbBlack&bbKnight))) & bbWhite & bbKnight,
-		^bitboard(bits.ReverseBytes64(uint64(bbWhite&bbBishop))) & bbBlack & bbBishop,
-		^bitboard(bits.ReverseBytes64(uint64(bbBlack&bbBishop))) & bbWhite & bbBishop,
-		^bitboard(bits.ReverseBytes64(uint64(bbWhite&bbRook))) & bbBlack & bbRook,
-		^bitboard(bits.ReverseBytes64(uint64(bbBlack&bbRook))) & bbWhite & bbRook,
-		bbBlack & bbQueen,
-		bbWhite & bbQueen,
-	} {
-		piece := Piece(p + 2)
-		for ; bb > 0; bb = bb.resetLSB() {
-			cb(piece, bb.scanForward())
-		}
-	}
 }
 
 // String implements the Stringer interface.
--- chess/position_test.go
+++ chess/position_test.go
@@ -2,7 +2,6 @@ package chess
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -69,105 +68,6 @@ func TestPosition_UnmakeMove(t *testing.T) {
 			pos.UnmakeMove(tt.move, meta, hash, pawnHash)
 			assert.Equal(t, tt.preFEN, pos.String())
 			assert.Equal(t, pre.hash, pos.hash)
-		})
-	}
-}
-
-func TestCountPieces(t *testing.T) {
-	pos := unsafeFEN(startFEN)
-	knights, bishops, rooks, queens := pos.CountPieces()
-	assert.Equal(t, 4, knights)
-	assert.Equal(t, 4, bishops)
-	assert.Equal(t, 4, rooks)
-	assert.Equal(t, 2, queens)
-}
-
-var pieceMapTests = []struct {
-	name         string
-	fen          string
-	uniquePieces map[Square]Piece
-	allPieces    map[Square]Piece
-}{
-	{
-		name: "starting position",
-		fen:  startFEN,
-		uniquePieces: map[Square]Piece{
-			D1: WhiteQueen, D8: BlackQueen,
-		},
-		allPieces: map[Square]Piece{
-			A8: BlackRook, B8: BlackKnight, C8: BlackBishop, D8: BlackQueen,
-			F8: BlackBishop, G8: BlackKnight, H8: BlackRook,
-			A1: WhiteRook, B1: WhiteKnight, C1: WhiteBishop, D1: WhiteQueen,
-			F1: WhiteBishop, G1: WhiteKnight, H1: WhiteRook,
-		},
-	},
-	{
-		name: "partial mirror",
-		fen:  "r1bq1rk1/pppp1ppp/2nb1n2/1B2p3/4P3/P1NP1N2/1PP2PPP/R1BQK2R w KQ - 0 1",
-		uniquePieces: map[Square]Piece{
-			D1: WhiteQueen, D8: BlackQueen,
-			H1: WhiteRook, F8: BlackRook,
-			B5: WhiteBishop, D6: BlackBishop,
-		},
-		allPieces: map[Square]Piece{
-			A8: BlackRook, C6: BlackKnight, C8: BlackBishop, D8: BlackQueen,
-			D6: BlackBishop, F6: BlackKnight, F8: BlackRook,
-			A1: WhiteRook, C3: WhiteKnight, C1: WhiteBishop, D1: WhiteQueen,
-			B5: WhiteBishop, F3: WhiteKnight, H1: WhiteRook,
-		},
-	},
-}
-
-func TestPieceMap(t *testing.T) {
-	for _, tt := range pieceMapTests {
-		t.Run(tt.name, func(t *testing.T) {
-			pos := unsafeFEN(tt.fen)
-			var pieces int
-			pos.PieceMap(func(p Piece, sq Square) {
-				pieces++
-				assert.Equal(t, tt.allPieces[sq], p, fmt.Sprintf("%v:%v", sq.String(), p.String()))
-			})
-			assert.Equal(t, len(tt.allPieces), pieces)
-		})
-	}
-}
-
-func BenchmarkPieceMap(b *testing.B) {
-	for _, bb := range pieceMapTests {
-		b.Run(bb.name, func(b *testing.B) {
-			pos := unsafeFEN(bb.fen)
-			for n := 0; n < b.N; n++ {
-				pos.PieceMap(func(p Piece, sq Square) {
-					_ = 1
-				})
-			}
-		})
-	}
-}
-
-func TestUniquePieceMap(t *testing.T) {
-	for _, tt := range pieceMapTests {
-		t.Run(tt.name, func(t *testing.T) {
-			pos := unsafeFEN(tt.fen)
-			var pieces int
-			pos.UniquePieceMap(func(p Piece, sq Square) {
-				pieces++
-				assert.Equal(t, tt.uniquePieces[sq], p, fmt.Sprintf("%v:%v", sq.String(), p.String()))
-			})
-			assert.Equal(t, len(tt.uniquePieces), pieces)
-		})
-	}
-}
-
-func BenchmarkUniquePieceMap(b *testing.B) {
-	for _, bb := range pieceMapTests {
-		b.Run(bb.name, func(b *testing.B) {
-			pos := unsafeFEN(bb.fen)
-			for n := 0; n < b.N; n++ {
-				pos.UniquePieceMap(func(p Piece, sq Square) {
-					_ = 1
-				})
-			}
 		})
 	}
 }
