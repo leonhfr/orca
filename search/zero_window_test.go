0c2e8aec6aa39244386c0cc994c8fe701f011dbe
Léon Hollender <hello@leonh.fr>
2023-06-08 06:58:06 +0200

    feat: zero window search

--- search/zero_window_test.go
+++ search/zero_window_test.go
@@ -6,60 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/leonhfr/orca/chess"
 )
-
-func (si *searchInfo) zeroWindow(ctx context.Context, pos *chess.Position, beta int32, depth uint8) (int32, error) {
-	select {
-	case <-ctx.Done():
-		return 0, context.Canceled
-	default:
-		si.nodes++
-	}
-
-	if pos.HasInsufficientMaterial() {
-		return draw, nil
-	}
-
-	checkData, inCheck := pos.InCheck()
-	if inCheck {
-		depth++
-	}
-
-	if depth == 0 {
-		return si.quiesce(ctx, pos, beta-1, beta)
-	}
-
-	hash := pos.Hash()
-	pawnHash := pos.PawnHash()
-	moves := pos.PseudoMoves(checkData)
-	quickScoreMoves(moves)
-
-	for i := 0; i < len(moves); i++ {
-		nextOracle(moves, i)
-		move := moves[i]
-
-		metadata, ok := pos.MakeMove(move)
-		if !ok {
-			continue
-		}
-
-		score, err := si.zeroWindow(ctx, pos, 1-beta, depth-1)
-		if err != nil {
-			return 0, err
-		}
-		score = -score
-
-		pos.UnmakeMove(move, metadata, hash, pawnHash)
-
-		if score >= beta {
-			return beta, nil
-		}
-	}
-
-	return beta - 1, nil
-}
 
 func TestZeroWindow(t *testing.T) {
 	for _, tt := range searchTestPositions {
