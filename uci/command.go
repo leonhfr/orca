88d3294d705dbccbf3093ac83b8379ae37b73705
Léon Hollender <hello@leonh.fr>
2023-07-03 21:51:51 +0200

    refactor: remove uci comments

--- uci/command.go
+++ uci/command.go
@@ -15,16 +15,6 @@ type command interface {
 }
 
 // commandUCI represents a "uci" command.
-//
-// Tell engine to use the uci (universal chess interface),
-// this will be sent once as a first command after program boot
-// to tell the engine to switch to uci mode.
-//
-// After receiving the uci command the engine must identify itself with the "id" command
-// and send the "option" commands to tell the GUI which engine settings the engine supports if any.
-//
-// After that the engine should send "uciok" to acknowledge the uci mode.
-// If no "uciok" is sent within a certain time period, the engine task will be killed by the GUI.
 type commandUCI struct{}
 
 // run implements the command interface.
@@ -42,13 +32,6 @@ func (commandUCI) run(_ context.Context, e Engine, c *Controller) {
 }
 
 // commandDebug represents a "debug" command.
-//
-// Switch the debug mode of the engine on and off.
-// In debug mode the engine should send additional infos to the GUI, e.g. with the "info string" command,
-// to help debugging, e.g. the commands that the engine has received etc.
-//
-// This mode should be switched off by default and this command can be sent
-// any time, also when the engine is thinking.
 type commandDebug struct {
 	on bool
 }
@@ -60,18 +43,6 @@ func (cmd commandDebug) run(_ context.Context, _ Engine, s *Controller) {
 }
 
 // commandIsReady represents an "isready" command.
-//
-// This is used to synchronize the engine with the GUI. When the GUI has sent a command or
-// multiple commands that can take some time to complete,
-// this command can be used to wait for the engine to be ready again or
-// to ping the engine to find out if it is still alive.
-// E.g. this should be sent after setting the path to the tablebases as this can take some time.
-//
-// This command is also required once before the engine is asked to do any search
-// to wait for the engine to finish initializing.
-//
-// This command must always be answered with "readyok" and can be sent also when the engine is calculating
-// in which case the engine should also immediately answer with "readyok" without stopping the search.
 type commandIsReady struct{}
 
 // run implements the command interface.
@@ -86,23 +57,6 @@ func (commandIsReady) run(_ context.Context, e Engine, c *Controller) {
 }
 
 // commandSetOption represents a "setoption" command.
-//
-// This is sent to the engine when the user wants to change the internal parameters
-// of the engine. For the "button" type no value is needed.
-//
-// One string will be sent for each parameter and this will only be sent when the engine is waiting.
-// The name and value of the option in <id> should not be case sensitive and can include spaces.
-//
-// The substrings "value" and "name" should be avoided in <id> and <x> to allow unambiguous parsing,
-// for example do not use <name> = "draw value".
-//
-// Here are some strings for the example below:
-//
-//	setoption name Nullmove value true\n
-//	setoption name Selectivity value 3\n
-//	setoption name Style value Risky\n
-//	setoption name Clear Hash\n
-//	setoption name NalimovPath value c:\chess\tb\4;c:\chess\tb\5\n
 type commandSetOption struct {
 	name  string
 	value string
@@ -116,17 +70,6 @@ func (cmd commandSetOption) run(_ context.Context, e Engine, c *Controller) {
 }
 
 // commandUCINewGame represents a "ucinewgame" command.
-//
-// This is sent to the engine when the next search (started with "position" and "go") will be from
-// a different game. This can be a new game the engine should play or a new game it should analyze but
-// also the next position from a testsuite with positions only.
-//
-// If the GUI hasn't sent a "ucinewgame" before the first "position" command, the engine shouldn't
-// expect any further ucinewgame commands as the GUI is probably not supporting the ucinewgame command.
-// So the engine should not rely on this command even though all new GUIs should support it.
-//
-// As the engine's reaction to "ucinewgame" can take some time the GUI should always send "isready"
-// after "ucinewgame" to wait for the engine to finish its operation.
 type commandUCINewGame struct{}
 
 // run implements the command interface.
@@ -141,16 +84,6 @@ func (commandUCINewGame) run(_ context.Context, e Engine, c *Controller) {
 }
 
 // commandPosition represents a "position" command.
-//
-//	position [fen <fenstring> | startpos ] moves <move1> ... <move i>
-//
-// Set up the position described in fenstring on the internal board and
-// play the moves on the internal chess board.
-//
-// If the game was played  from the start position the string "startpos" will be sent.
-//
-// Note: no "new" command is needed. However, if this position is from a different game than
-// the last position sent to the engine, the GUI should have sent a "ucinewgame" in between.
 type commandPosition struct {
 	fen      string
 	moves    []string
@@ -187,72 +120,6 @@ func (cmd commandPosition) run(_ context.Context, _ Engine, c *Controller) {
 }
 
 // commandGo represents a "go" command.
-//
-// Start calculating on the current position set up with the "position" command.
-//
-// There are a number of commands that can follow this command, all will be sent in the same string.
-// If one command is not sent its value should be interpreted as it would not influence the search.
-//
-//	searchmoves <move1> ... <move i>
-//
-// Restrict search to this moves only.
-//
-// Example: After "position startpos" and "go infinite searchmoves e2e4 d2d4"
-// the engine should only search the two moves e2e4 and d2d4 in the initial position.
-//
-//	ponder
-//
-// Start searching in pondering mode.
-//
-// Do not exit the search in ponder mode, even if it's mate!
-// This means that the last move sent in in the position string is the ponder move.
-// The engine can do what it wants to do, but after a "ponderhit" command
-// it should execute the suggested move to ponder on. This means that the ponder move sent by
-// the GUI can be interpreted as a recommendation about which move to ponder. However, if the
-// engine decides to ponder on a different move, it should not display any mainlines as they are
-// likely to be misinterpreted by the GUI because the GUI expects the engine to ponder
-// on the suggested move.
-//
-//	wtime <x>
-//
-// White has x msec left on the clock.
-//
-//	btime <x>
-//
-// Black has x msec left on the clock.
-//
-//	winc <x>
-//
-// White increment per move in milliseconds if x > 0.
-//
-//	binc <x>
-//
-// Black increment per move in milliseconds if x > 0.
-//
-//	movestogo <x>
-//
-// There are x moves to the next time control, this will only be sent if x > 0.
-// If you don't get this and get the wtime and btime it's sudden death.
-//
-//	depth <x>
-//
-// Search x plies only.
-//
-//	nodes <x>
-//
-// Search x nodes only.
-//
-//	mate <x>
-//
-// Search for a mate in x moves.
-//
-//	movetime <x>
-//
-// Search exactly x milliseconds.
-//
-//	infinite
-//
-// Search until the "stop" command. Do not exit the search without being told so in this mode!
 //
 //nolint:govet
 type commandGo struct {
@@ -314,9 +181,6 @@ func searchContext(ctx context.Context, stop <-chan struct{}, limit time.Duratio
 }
 
 // commandStop represents a "stop" command.
-//
-// Stop calculating as soon as possible,
-// don't forget the "bestmove" and possibly the "ponder" token when finishing the search.
 type commandStop struct{}
 
 // run implements the command interface.
@@ -328,8 +192,6 @@ func (commandStop) run(_ context.Context, _ Engine, c *Controller) {
 }
 
 // commandQuit represents a "quit" command.
-//
-// Quit the program as soon as possible.
 type commandQuit struct{}
 
 // run implements the command interface.
--- uci/response.go
+++ uci/response.go
@@ -15,16 +15,6 @@ type response interface {
 }
 
 // responseID represents a "id" command.
-//
-//	name <x>
-//
-// This must be sent after receiving the "uci" command to identify the engine,
-// e.g. "id name Shredder X.Y\n".
-//
-//	author <x>
-//
-// This must be sent after receiving the "uci" command to identify the engine,
-// e.g. "id author Stefan MK\n".
 type responseID struct {
 	name   string
 	author string
@@ -35,9 +25,6 @@ func (r responseID) format(_ *Controller) string {
 }
 
 // responseUCIOK represents a "uciok" command.
-//
-// Must be sent after the id and optional options to tell the GUI that the engine
-// has sent all infos and is ready in uci mode.
 type responseUCIOK struct{}
 
 func (r responseUCIOK) format(_ *Controller) string {
@@ -45,13 +32,6 @@ func (r responseUCIOK) format(_ *Controller) string {
 }
 
 // responseReadyOK represents a "readyok" command.
-//
-// This must be sent when the engine has received an "isready" command and has
-// processed all input and is ready to accept new commands now.
-//
-// It is usually sent after a command that can take some time to be able to wait for the engine,
-// but it can be used anytime, even when the engine is searching,
-// and must always be answered with "isready".
 type responseReadyOK struct{}
 
 func (r responseReadyOK) format(_ *Controller) string {
@@ -59,16 +39,6 @@ func (r responseReadyOK) format(_ *Controller) string {
 }
 
 // responseBestMove represents a "bestmove" command.
-//
-//	bestmove <move1> [ ponder <move2> ]
-//
-// The engine has stopped searching and found the move <move> best in this position.
-// The engine can send the move it likes to ponder on. The engine must not start pondering automatically.
-//
-// This command must always be sent if the engine stops searching, also in pondering mode if there is a
-// "stop" command, so for every "go" command a "bestmove" command is needed!
-// Directly before that the engine should send a final "info" command with the final search information,
-// the the GUI has the complete statistics about the last search.
 type responseBestMove struct {
 	move chess.Move
 }
@@ -78,111 +48,6 @@ func (r responseBestMove) format(c *Controller) string {
 }
 
 // responseInfo represents an "info" command.
-//
-// The engine wants to send information to the GUI. This should be done whenever one of the info has changed.
-// The engine can send only selected infos or multiple infos with one info command, e.g.
-//
-//	info currmove e2e4 currmovenumber 1
-//	info depth 12 nodes 123456 nps 100000
-//
-// Also all infos belonging to the pv should be sent together e.g.
-//
-//	info depth 2 score cp 214 time 1242 nodes 2124 nps 34928 pv e2e4 e7e5 g1f3
-//
-// I suggest to start sending "currmove", "currmovenumber", "currline" and "refutation" only after one second
-// to avoid too much traffic.
-// Additional info:
-//
-//	depth <x>
-//
-// Search depth in plies.
-//
-//	seldepth <x>
-//
-// Selective search depth in plies,
-// if the engine sends seldepth there must also be a "depth" present in the same string.
-//
-//	time <x>
-//
-// The time searched in ms, this should be sent together with the pv.
-//
-//	nodes <x>
-//
-// x nodes searched, the engine should send this info regularly.
-//
-//	pv <move1> ... <move i>
-//
-// The best line found.
-//
-//	multipv <num>
-//
-// This for the multi pv mode.
-// For the best move/pv add "multipv 1" in the string when you send the pv.
-// In k-best mode always send all k variants in k strings together.
-//
-//	score
-//
-// There are four possibilities:
-//
-//   - cp <x>: the score from the engine's point of view in centipawns
-//
-//   - mate <y>: mate in y moves, not plies. If the engine is getting mated use negative values for y
-//
-//   - lowerbound: the score is just a lower bound
-//
-//   - upperbound: the score is just an upper bound
-//
-//     currmove <move>
-//
-// Currently searching this move.
-//
-//	currmovenumber <x>
-//
-// Currently searching move number x, for the first move x should be 1 not 0.
-//
-//	hashfull <x>
-//
-// The hash is x permill full, the engine should send this info regularly.
-//
-//	nps <x>
-//
-// x nodes per second searched, the engine should send this info regularly.
-//
-//	tbhits <x>
-//
-// x positions where found in the endgame table bases.
-//
-//	sbhits <x>
-//
-// x positions where found in the shredder endgame databases.
-//
-//	cpuload <x>
-//
-// The cpu usage of the engine is x permill.
-//
-//	string <str>
-//
-// Any string str which will be displayed be the engine,
-// if there is a string command the rest of the line will be interpreted as <str>.
-//
-//	refutation <move1> <move2> ... <move i>
-//
-// Move <move1> is refuted by the line <move2> ... <move i>, i can be any number >= 1.
-//
-// Example: after move d1h5 is searched, the engine can send
-// "info refutation d1h5 g6h5"
-// if g6h5 is the best answer after d1h5 or if g6h5 refutes the move d1h5.
-// If there is no refutation for d1h5 found, the engine should just send
-// "info refutation d1h5".
-// The engine should only send this if the option "UCI_ShowRefutations" is set to true.
-//
-//	currline <cpunr> <move1> ... <move i>
-//
-// This is the current line the engine is calculating. <cpunr> is the number of the cpu if
-// the engine is running on more than one cpu. <cpunr> = 1,2,3...
-// If the engine is just using one cpu, <cpunr> can be omitted.
-// If <cpunr> is greater than 1, always send all k lines in k strings together.
-// The engine should only send this if the option "UCI_ShowCurrLine" is set to true.
 type responseOutput struct {
 	Output
 	time time.Duration
@@ -216,73 +81,6 @@ func (o responseOutput) format(c *Controller) string {
 }
 
 // Option represents an "option" command.
-//
-// This command tells the GUI which parameters can be changed in the engine.
-// This should be sent once at engine startup after the "uci" and the "id" commands
-// if any parameter can be changed in the engine.
-// The GUI should parse this and build a dialog for the user to change the settings.
-//
-// Note that not every option needs to appear in this dialog as some options like
-// "Ponder", "UCI_AnalyseMode", etc. are better handled elsewhere or are set automatically.
-// If the user wants to change some settings, the GUI will send a "setoption" command to the engine.
-//
-// Note that the GUI need not send the setoption command when starting the engine for every option if
-// it doesn't want to change the default value.
-// For all allowed combinations see the examples below,
-// as some combinations of this tokens don't make sense.
-// One string will be sent for each parameter.
-//
-//	name <id>
-//
-// The option has the name id.
-//
-// Certain options have a fixed value for <id>, which means that the semantics of this option is fixed.
-// Usually those options should not be displayed in the normal engine options window of the GUI but
-// get a special treatment. "Pondering" for example should be set automatically when pondering is
-// enabled or disabled in the GUI options. The same for "UCI_AnalyseMode" which should also be set
-// automatically by the GUI. All those certain options have the prefix "UCI_" except for the
-// first 6 options below. If the GUI gets an unknown Option with the prefix "UCI_", it should just
-// ignore it and not display it in the engine's options dialog.
-//
-//	type <t>
-//
-// The option has type t.
-// There are 5 different types of options the engine can send
-//
-//   - check: a checkbox that can either be true or false
-//
-//   - spin: a spin wheel that can be an integer in a certain range
-//
-//   - combo: a combo box that can have different predefined strings as a value
-//
-//   - button: a button that can be pressed to send a command to the engine
-//
-//   - string: a text field that has a string as a value, an empty string has the value "<empty>"
-//
-//     default <x>
-//
-// The default value of this parameter is x.
-//
-//	min <x>
-//
-// The minimum value of this parameter is x.
-//
-//	max <x>
-//
-// The maximum value of this parameter is x.
-//
-//	var <x>
-//
-// A predefined value of this parameter is x.
-//
-// Examples:
-//
-// Here are 5 strings for each of the 5 possible types of options
-//   - "option name Nullmove type check default true\n"
-//   - "option name Selectivity type spin default 2 min 0 max 4\n"
-//   - "option name Style type combo default Normal var Solid var Normal var Risky\n"
-//   - "option name NalimovPath type string default c:\\n"
-//   - "option name Clear Hash type button\n"
 func (o Option) format(_ *Controller) string {
 	switch o.Type {
 	case OptionInteger:
